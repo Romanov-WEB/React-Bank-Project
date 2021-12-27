b176a0f595bf3edcd633cd1f35b8d2a68396aee6
Zhenya <asgyrdjons@gmail.com>
2021-12-28 00:11:52 +0300

    card-selection

--- src/pages/payments/component/BlockLeftHeader.jsx
+++ src/pages/payments/component/BlockLeftHeader.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
+import dots from 'assets/icon/global/dots-icon.svg'
 
 export default function BlockLeftHeader() {
     console.log('Render BlockLeftHeader');
     return (
         <div className="payment__header">
             <p className="payment__title">Моментальный перевод</p>
-            <img src="img/icon/dots-icon.svg" className="payment__more__icon" alt={'dots-icon.svg'} />
+            <img src={dots} className="payment__more__icon" alt={'dots-icon.svg'} />
         </div>
     )
 }
+React.memo(BlockLeftHeader, ()=> {
+    return true
+})
